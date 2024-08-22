475a4b85f8314fa723b0d460b5e754e9b12b01a8
lawrencejrhee <lawrencejrhee@gmail.com>
2024-08-21 18:54:28 -0700

    Added Contact Us Page
    Added the basic page for Contact Us, need to add more details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ContactUs } from "@/components/ContactUs/ContactUs";
 import { HeroImageRight } from "@/components/HeroImageRight";
 import ProjectCard from "@/components/ProjectCard";
 import {
@@ -17,6 +18,12 @@ export default function Home() {
   return (
     <Stack>
       <HeroImageRight />
+      <Text>
+        Hello world 
+      </Text>
+      <ContactUs>
+        
+      </ContactUs>
     </Stack>
   );
 }
